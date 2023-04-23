bf1768452014fbfe487ea1e5f4f4f5274cc3d9ef
sajansiwa <saj.siwa@gmail.com>
2023-04-23 19:20:53 +0545

    new branch

---
+++ Coursework.Application/DTO/BookCarRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coursework.Application.DTO
+{
+    public class BookCarRequestDTO
+    {
+        public string customer { get; set; }
+        public string CarId { get; set; }
+        public DateTime RentStartdate { get; set; }
+        public DateTime RentEnddate { get; set; }
+
+    }
+}
---
+++ Coursework.Infrastructure/Services/BookCarService.cs
@@ -0,0 +1,30 @@
+﻿using Coursework.Application.Common.Interface;
+using Coursework.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coursework.Infrastructure.Services
+{
+    public class BookCarService : IBookCar
+
+
+    {
+        private readonly IApplicationDBContext _dbContext;
+        public BookCarService(IApplicationDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+
+        public Task<ResponseDTO> BookCarRequest(BookCarRequestDTO model)
+        {
+            try {
+
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
