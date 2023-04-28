3be8160cd2231c3e074f5cf839dfd6250dfe176e
sajansiwa <saj.siwa@gmail.com>
2023-04-28 14:50:25 +0545

    check date availability and necessary debugging

--- Coursework.Domain/Entities/CustomerBooking.cs
+++ Coursework.Domain/Entities/CustomerBooking.cs
@@ -19,5 +19,9 @@ namespace Coursework.Domain.Entities
         public DateTime RentStartdate { get; set; }
         public DateTime RentEnddate { get; set;}
         public Boolean? IsApproved { get; set; } 
+        public Boolean? rented { get; set; } 
+        public Boolean? payment { get; set; } 
+
+       
     }
 }
